06356e22412ed4402bd44840fdfac632db5b51e2
= <=>
2024-08-28 20:52:51 -0300

    Criando services, interfaces, dominios e conexão

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using minimal_api.Dominio.DTOs;
+using minimal_api.Dominio.Entidades;
+using minimal_api.Dominio.Interfaces;
+using minimal_api.Infraestrutura.Db;
+using minimal_api.Infraestrutura.Servicos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
 app.MapGet("/", () => "Hello World!");
+
+builder.Services.AddDbContext<DbContexto>( options => {
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+        );
+    }
+);
+
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+    if(administradorServico.Login(loginDTO) != null){
+        return Results.Ok("Login com sucesso");
+    }
+    return Results.Unauthorized();
+} );
+
+
 
 app.Run();
